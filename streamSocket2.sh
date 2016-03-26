667045c25bc8ab90ea62e7be3f00ada370392852
Omar Backhoff <omarbackhoff@gmail.com>
2016-03-26 21:45:44 +0100

    whatever shieeet

--- streamSocket2.sh
+++ streamSocket2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DIR_STREAM=/home/omar/stream
-DIR_STREAM_SPLIT="/home/omar/datasets/split2"
+DIR_STREAM=/home/omar/stream/split2
+DIR_STREAM_SPLIT="/home/omar/datasets/split"
 CNT=0
-TIME=0.05
-L2OOP=1
+TIME=0.09
+LOOP=1
 
 ##  use this frst:  nc -lk 9998 | nc -lk 9999
 
-#echo "LOOP = $LOOP"
+# echo "LOOP = $LOOP"
 while [ -f $DIR_STREAM_SPLIT/$CNT ]; do
     cat $DIR_STREAM_SPLIT/$CNT | nc localhost 9998
     echo "Streamed file no. $CNT"
